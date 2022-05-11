86bdec74ed37edbe30eed4eafb7880d8543cdde2
labdaddy <47070992+labdaddy@users.noreply.github.com>
2022-05-11 14:24:05 -0700

    Update bmi_calculator.py

--- bmi_calculator.py
+++ bmi_calculator.py
@@ -1,15 +1,15 @@
-`height = input("enter your height in m: ")`
-`weight = input("enter your weight in kg: ")`
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
 
-`bmi = float(weight) / (float(height) * float(height))`
-`cleaned_bmi = int(bmi)`
-`print(cleaned_bmi)`
+bmi = float(weight) / (float(height) * float(height))
+cleaned_bmi = int(bmi)
+print(cleaned_bmi)
 
 ### Refactored
 
-`height = input("enter your height in m: ")`
-`weight = input("enter your weight in kg: ")`
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
 
-`bmi = int(weight) / float(height) ** 2`
-`bmi_as_int = int(bmi)`
-`print(bmi_as_int)`
+bmi = int(weight) / float(height) ** 2
+bmi_as_int = int(bmi)
+print(bmi_as_int)
