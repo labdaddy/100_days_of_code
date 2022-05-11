4a60bfdebbbe5f158979c4c1ba2941f299b50162
labdaddy <47070992+labdaddy@users.noreply.github.com>
2022-05-11 16:23:50 -0700

    Update life_in_weeks.py

--- life_in_weeks.py
+++ life_in_weeks.py
@@ -17,4 +17,5 @@ months_remaining = years_remaining * 12
 weeks_remaining = years_remaining * 52
 days_remaining = years_remaining * 365
 
-print(f"You have {days_remaining} days, {weeks_remaining} weeks, and {months_remaining} months left.")
+message = f"You have {days_remaining} days, {weeks_remaining} weeks, and {months_remaining} months left."
+print(message)
